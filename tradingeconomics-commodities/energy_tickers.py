94e645526945796224c1ee0c6409e2d2a99d1b30
SJ Lee <rpslack@gmail.com>
2026-05-14 13:40:41 +0900

    Delete tradingeconomics-commodities directory

--- tradingeconomics-commodities/energy_tickers.py
+++
@@ -1,5 +0,0 @@
-"""Backward-compatible re-exports for Energy-only mappings."""
-
-from commodity_tickers import ENERGY_NAME_TO_TICKER, ENERGY_TICKER_TO_NAME
-
-__all__ = ["ENERGY_NAME_TO_TICKER", "ENERGY_TICKER_TO_NAME"]
